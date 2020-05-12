47da5a3b1d085070c5261fd6d96d281f9fbd8a46
pedrohcalado <pedrocalado22@gmail.com>
2020-05-12 18:12:26 -0300

    Exercise 05

---
+++ 4_1-javaScript_first_steps/exercises/05.js
@@ -0,0 +1,10 @@
+//Faça um programa que defina três variáveis com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false caso contrário.
+
+//Para os ângulos serem de um triângulo válido, a soma dos três deve ser 180 graus. Caso os ângulos estejam inválidos, o programa deve retornar uma mensagem de erro.
+
+let a = 60;
+let b = 50;
+let c = 74;
+
+(a+b+c==180)?console.log(true):console.log(false);
+
