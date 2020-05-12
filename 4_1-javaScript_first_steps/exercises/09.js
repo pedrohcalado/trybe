93c651536ed4fffbd80378f17b504edbadaa3d12
pedrohcalado <pedrocalado22@gmail.com>
2020-05-12 18:37:32 -0300

    Exercise 09

---
+++ 4_1-javaScript_first_steps/exercises/09.js
@@ -0,0 +1,11 @@
+//Escreva um programa que defina três números em variáveis no seu começo e retorne true se uma das três for ímpar. Caso contrário, ele retorna false.
+
+let a = 14;
+let b = 2;
+let c = 6;
+
+if (a%2!=0 || b%2!=0 || c%2!=0){
+    console.log(true);
+} else {
+    console.log(false);
+}
