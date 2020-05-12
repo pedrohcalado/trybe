c3585849871fbf82b5c8055aa751518a6ca1467a
pedrohcalado <pedrocalado22@gmail.com>
2020-05-12 18:26:25 -0300

    Exercise 06

---
+++ 4_1-javaScript_first_steps/exercises/06.js
@@ -0,0 +1,10 @@
+let peca = "PAWN";
+
+switch (peca.toLowerCase()){
+    case "bishop":
+        console.log("diagonals");
+    case "knight":
+        console.log("L movements");
+    case "pawn":
+        console.log("to the sides os front");
+}
