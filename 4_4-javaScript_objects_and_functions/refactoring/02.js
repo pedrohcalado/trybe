cd10de6c53bd390bc5224f8828f5f7cb4b0e4c89
pedrohcalado <pedrocalado22@gmail.com>
2020-05-15 15:15:28 -0300

    Exercise 02 - refactoring

--- 4_4-javaScript_objects_and_functions/refactoring/02.js
+++ 4_4-javaScript_objects_and_functions/refactoring/02.js
@@ -1,14 +1,15 @@
 //Faça um programa que retorne o maior de dois números. Defina no começo do programa duas variáveis com os valores que serão comparados.
 
-let a = 1;
-let b = 2;
+function higher(a,b) {
+  if (a>b){
+    console.log(a);
+  }
+  else if (a<b) {
+      console.log(b);
+  }
+  else {
+      console.log ("Os números são iguais");
+  }
+}
 
-if (a>b){
-    return a;
-}
-else if (a<b) {
-    return b;
-}
-else {
-    console.log ("Os números são iguais");
-}
+higher(5,4);
