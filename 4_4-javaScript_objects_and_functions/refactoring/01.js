49ddd67aaaaa2d3922914d43b31242aa911b5f67
pedrohcalado <pedrocalado22@gmail.com>
2020-05-15 15:10:53 -0300

    Exercise 01 - refactoring

--- 4_4-javaScript_objects_and_functions/refactoring/01.js
+++ 4_4-javaScript_objects_and_functions/refactoring/01.js
@@ -7,10 +7,8 @@ Divisão
 Módulo
 */
 
-let a = 1;
-let b = 2;
-
-let soma = a + b;
+function operations(a,b) {
+  let soma = a + b;
 let sub = a - b;
 let mult = a * b;
 let div = a / b;
@@ -21,3 +19,7 @@ console.log("A subtração é: " + sub);
 console.log("A multiplicação é: " + mult);
 console.log("A divisão é: " + div);
 console.log("O módulo é: " + mod);
+}
+
+operations(4,3);
+
