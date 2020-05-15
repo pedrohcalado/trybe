577d0e0408d7ec93990074bbe4c86a1df38a87f4
pedrohcalado <pedrocalado22@gmail.com>
2020-05-15 07:58:52 -0300

    Adjust exercise 04 to work with even numbers as well

--- 4_3-javaScript_algorithms/04.js
+++ 4_3-javaScript_algorithms/04.js
@@ -1,18 +1,34 @@
-function invertedTriangle(n) {
-    if (n >1 && Number.isInteger(n) && n%2 != 0) {
+function triangle(n) {
+    if (n >1 && Number.isInteger(n)) {
         let espace = "";
-        let asterisk = "*";
-        for (let i = 1;i <= n;i+=2) {
+        let asterisk = "";
+        if (n%2 == 0) {
             espace = "";
-            for (let j = i;j < n;j+=2) {
-                espace += " ";
+            asterisk = "**";
+            for (let i = 1;i <= n;i+=2) {
+                espace = "";
+                for (let j = i;j < n - 1;j+=2) {
+                    espace += " ";
+                }
+                console.log(espace + asterisk + espace);
+                asterisk += "**";
             }
-            console.log(espace + asterisk + espace);
-            asterisk += "**";
-        }
+        } else {
+            espace = "";
+            asterisk = "*";
+            for (let i = 1;i <= n;i+=2) {
+                espace = "";
+                for (let j = i;j < n;j+=2) {
+                    espace += " ";
+                }
+                console.log(espace + asterisk + espace);
+                asterisk += "**";
+            }
+        }   
     } else {
         console.log("O número precisa ser inteiro, ímpar e maior que 1");
     }
 }
 
-invertedTriangle(6);
+triangle(5);
+triangle(6);
