268ec54786754f8b5b8384c9cd85a4fb1a59f625
pedrohcalado <pedrocalado22@gmail.com>
2020-05-15 15:29:40 -0300

    Exercise 06 - refactoring

--- 4_4-javaScript_objects_and_functions/refactoring/06.js
+++ 4_4-javaScript_objects_and_functions/refactoring/06.js
@@ -1,12 +1,17 @@
 //Escreva um programa que receba o nome de uma peça de xadrez e retorne os movimentos que ela faz.
 
-let peca = "PAWN";
-
-switch (peca.toLowerCase()){
+function chess(peca) {
+  switch (peca.toLowerCase()){
     case "bishop":
         console.log("diagonals");
+        break;
     case "knight":
         console.log("L movements");
+        break;
     case "pawn":
         console.log("to the sides os front");
+        break; 
+  }  
 }
+
+chess("bishop");
