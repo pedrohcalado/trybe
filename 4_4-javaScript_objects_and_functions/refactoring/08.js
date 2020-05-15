c9ba7e309a02b64a5b594dccc2f46b694c379cc6
pedrohcalado <pedrocalado22@gmail.com>
2020-05-15 15:32:32 -0300

    Exercise 08 - refactoring

--- 4_4-javaScript_objects_and_functions/refactoring/08.js
+++ 4_4-javaScript_objects_and_functions/refactoring/08.js
@@ -1,11 +1,12 @@
 //Escreva um programa que defina três números em variáveis no seu começo e retorne true se uma das três for par. Caso contrário, ele retorna false.
 
-let a = 1;
-let b = 1;
-let c = 3;
-
-if (a%2==0 || b%2==0 || c%2==0){
+function oneEven(a,b,c) {
+  if (a%2==0 || b%2==0 || c%2==0){
     console.log(true);
 } else {
     console.log(false);
 }
+}
+
+oneEven(1,2,3);
+oneEven(1,5,7);
