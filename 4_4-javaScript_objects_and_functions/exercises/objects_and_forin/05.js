93de1c22c80b0f198ec99db80f6dedf5ae3f2b2f
pedrohcalado <pedrocalado22@gmail.com>
2020-05-18 09:04:17 -0300

    Adjust exercise 05 - objects and for in

--- 4_4-javaScript_objects_and_functions/exercises/objects_and_forin/05.js
+++ 4_4-javaScript_objects_and_functions/exercises/objects_and_forin/05.js
@@ -1,15 +1,26 @@
 //Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: “Tio Patinhas”, “Christmas on Bear Mountain, Dell’s Four Color Comics #178”, “O último MacPatinhas”, “Sim”.
 
-let info = {
+//criando array de objetos
+let infos = [{
   personagem: "Margarida",
   origem: "Pato Donald",
   nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
   recorrente: "Sim"
-};
-
-let info2 = {
+},
+{
   personagem: "Tio Patinhas",
   origem: "Christmas on Bear Mountain, Dell’s Four Color Comics #178",
   nota: "O último MacPatinhas",
   recorrente: "Sim"
+}
+]
+//criando array de keys
+let keys = [];
+for (let i in infos[0]) {
+  keys.push(i);
+}
+
+//imprimindo valores
+for (let k in keys) {
+  console.log(`${infos[0][keys[k]]} e ${infos[1][keys[k]]}`);
 }
