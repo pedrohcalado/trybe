7f48e482fa3d2ff1faafcf1fde26a34290b6e592
pedrohcalado <pedrocalado22@gmail.com>
2020-05-29 09:06:28 -0300

    Add email validation

--- 6_1-HTML_CSS_forms/script.js
+++ 6_1-HTML_CSS_forms/script.js
@@ -1,6 +1,5 @@
 // add states to combobox
 const states = document.getElementById('states');
-
 const statesArray = ['<Nenhum>','AC','AL','AP','AM','BA','CE','DF','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO'];
 
 for (let i = 0; i < statesArray.length; i += 1) {
@@ -13,15 +12,26 @@ for (let i = 0; i < statesArray.length; i += 1) {
 // listen to the submit button
 const sendButton = document.getElementById('submit');
 const date = document.getElementById('start-date');
+const email = document.getElementById('email');
 sendButton.addEventListener('click', function (event) {
-  if (!testDate(date.value)) {
+  if (!validateDate(date.value)) {
     event.preventDefault();
     alert('Erro na data');
   }
+  if (!validateEmail(email.value)) {
+    event.preventDefault();
+    alert('Erro no email');
+  }
 })
 
-// test the date format
-function testDate (date) {
-  let date_regex = /^(0[1-9]|1\d|2\d|3[0-1])\/(0[1-9]|1[0-2])\/(19|20)\d{2}$/;
+// validate date format
+function validateDate (date) {
+  const date_regex = /^(0[1-9]|1\d|2\d|3[0-1])\/(0[1-9]|1[0-2])\/(19|20)\d{2}$/;
   return (date_regex.test(date));
+}
+
+// validate email format
+function validateEmail(email) {
+  const email_regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return email_regex.test(String(email).toLowerCase());
 }
