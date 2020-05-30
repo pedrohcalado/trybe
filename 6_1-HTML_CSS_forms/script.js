1ce96bea0f41614d22bcfd502a05962fa496e4b2
pedrohcalado <pedrocalado22@gmail.com>
2020-05-30 09:21:12 -0300

    Create json with the input data

--- 6_1-HTML_CSS_forms/script.js
+++ 6_1-HTML_CSS_forms/script.js
@@ -4,8 +4,8 @@ const statesArray = ['<Nenhum>','AC','AL','AP','AM','BA','CE','DF','ES','GO','MA
 
 for (let i = 0; i < statesArray.length; i += 1) {
   const eachState = document.createElement('option');
-  eachState.classList.add('state');
   eachState.innerHTML = statesArray[i];
+  eachState.value = statesArray[i];
   states.appendChild(eachState);
 }
 
@@ -13,6 +13,10 @@ for (let i = 0; i < statesArray.length; i += 1) {
 const sendButton = document.getElementById('submit');
 const date = document.getElementById('start-date');
 const email = document.getElementById('email');
+const inputs = document.querySelectorAll('input');
+const options = document.querySelectorAll('options');
+const textareas = document.querySelectorAll('textarea');
+
 sendButton.addEventListener('click', function (event) {
   if (!validateDate(date.value)) {
     event.preventDefault();
@@ -22,7 +26,26 @@ sendButton.addEventListener('click', function (event) {
     event.preventDefault();
     alert('Erro no email');
   }
+
+
+  // create an object with the data
+  const dataConsolidated = {};
+  for (let i = 0; i < inputs.length; i += 1) {
+    dataConsolidated[inputs[i].name] = inputs[i].value;
+  }
+  for (let i = 0; i < options.length; i += 1) {
+    dataConsolidated[options[i].name] = options[i].value;
+  }
+  for (let i = 0; i < textareas.length; i += 1) {
+    dataConsolidated[textareas[i].name] = textareas[i].value;
+  }
+  sessionStorage.setItem('data', JSON.stringify(dataConsolidated));
 })
+
+// create div with consolidated information
+window.onload = function () {
+  console.log(JSON.parse(sessionStorage.getItem('data')));
+}
 
 // validate date format
 function validateDate (date) {
@@ -35,3 +58,5 @@ function validateEmail(email) {
   const email_regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return email_regex.test(String(email).toLowerCase());
 }
+
+
