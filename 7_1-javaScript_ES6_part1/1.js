0b24aaa396791757155ab3483f10a57676b5e253
pedrohcalado <pedrocalado22@gmail.com>
2020-06-08 15:15:12 -0300

    done

--- 7_1-javaScript_ES6_part1/1.js
+++ 7_1-javaScript_ES6_part1/1.js
@@ -3,16 +3,3 @@ const factorial = (n) => (n > 0) ? (n * factorial(n-1)) : 1;
 
 console.log(factorial(3));
 
-// Crie uma função que receba uma frase e retorne qual a maior palavra.
-
-const largestWord = phrase => {
-  let arr = phrase.split(' ');
-  let largest = arr[0];
-  for (i in arr) {
-    (arr[i] > arr[0]) ? largest = arr[i] : largest;
-  }
-  return (largest);
-}
-
-console.log(largestWord('Antônio foi no banheiro e não sabemos o que aconteceu'));
-
