90e57a7632898695ec831b6630b96dad71d5e76c
pedrohcalado <pedrocalado22@gmail.com>
2020-06-08 18:52:38 -0300

    Exercises 3 and 4 - partII

--- 7_2-javaScript_ES6_objects/part2.js
+++ 7_2-javaScript_ES6_objects/part2.js
@@ -18,14 +18,14 @@ const lesson3 = {
   turno: 'noite',
 };
 
-// Crie uma função para adicionar o turno da manhã na lesson2. Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.
-const addShift = (lesson, k, v) => {
-  lesson[k] = v;
-};
+// 1. Crie uma função para adicionar o turno da manhã na lesson2. Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.
+const addShift = (lesson, k, v) => lesson[k] = v;
 
-// Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
+// 2. Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
+const listKeys = object => Object.keys(object);
 
-const listKeys = (object) => {
-  console.log(Object.keys(object));
-}
+// 3. Crie uma função para mostrar o tamanho de um objeto.
+const objectSize = object => Object.keys(object).length;
 
+// 4. Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro.
+const listValues = object => Object.values(object);
