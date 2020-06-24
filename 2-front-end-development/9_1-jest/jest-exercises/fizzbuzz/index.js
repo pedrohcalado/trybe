a479c111c626ea53ae4d75eee82ee92d3ae75e15
pedrohcalado <pedrocalado22@gmail.com>
2020-06-24 11:06:10 -0300

    fizzbuzz

--- 2-front-end-development/9_1-jest/jest-exercises/fizzbuzz/index.js
+++ 2-front-end-development/9_1-jest/jest-exercises/fizzbuzz/index.js
@@ -13,7 +13,13 @@
 //   buzz
 
 function fizzBuzz(n) {
-
+  for (let num = 1; num <= n; num += 1) {
+    if (typeof num !== 'number') console.log(false);
+    else if (num % 3 === 0 && num % 5 === 0) console.log('fizzbuzz');
+    else if (num % 3 === 0) console.log('fizz');
+    else if (num % 5 === 0) console.log('buzz');
+    else console.log(num);
+  }
 }
 
 module.exports = fizzBuzz;
