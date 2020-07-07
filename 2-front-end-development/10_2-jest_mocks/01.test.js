7170085f943c70d17ef30722df234471861af10a
pedrohcalado <pedrocalado22@gmail.com>
2020-07-07 07:56:49 -0300

    Exercise 01

---
+++ 2-front-end-development/10_2-jest_mocks/01.test.js
@@ -0,0 +1,15 @@
+// Crie uma função que gere um número aleatório entre 0 e 100. Você irá criar também os testes para essa função. Utilizando o mock, defina o retorno padrão como 10. Teste se a função foi chamada, qual seu retorno e quantas vezes foi chamada.
+
+function randomNumber() {
+  return Math.floor(Math.random() * 100);
+}
+
+test('testing the function mocking with 10', () => {
+  randomNumber = jest.fn().mockReturnValue(10);
+  
+  randomNumber();
+  expect(randomNumber).toHaveBeenCalled();
+  expect(randomNumber).toHaveBeenCalledTimes(1);
+  expect(randomNumber()).toBe(10);
+  expect(randomNumber).toHaveBeenCalledTimes(2);
+});
