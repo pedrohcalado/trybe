4e83e73fc54565a66af2d780ce30a42078d9e256
pedrohcalado <pedrocalado22@gmail.com>
2020-07-09 10:53:21 -0300

    Exercises

---
+++ 2-front-end-development/11_1-react_introduction/first-app/src/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const task = (value) => {
+  return (
+    <li>{value}</li>
+  );
+}
+
+const comp = ['comp1', 'comp2', 'comp3'];
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ol>
+          {comp.map(n => task(n))}
+        </ol>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+
+
+export default App;
