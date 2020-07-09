0ab9d2b4688ed3ac589157658eab5ec82f46f0c6
pedrohcalado <pedrocalado22@gmail.com>
2020-07-09 19:08:52 -0300

    Fixation exercises

--- 2-front-end-development/11_2-react_components/fixation-exercises-11-2/src/App.js
+++ 2-front-end-development/11_2-react_components/fixation-exercises-11-2/src/App.js
@@ -1,8 +1,45 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Image from './Image';
+import Order from './Order';
+import UserProfile from './UserProfile';
 
 function App() {
+  const users = [
+    {
+      id: 102,
+      name: "João",
+      email: "joao@gmail.com",
+      avatar: "https:cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_640.png"
+    },
+    {
+      id: 77,
+      name: "Amélia",
+      email: "amelia@gmail.com",
+      avatar: "https:cdn.pixabay.com/photo/2017/01/31/13/05/cameo-2023867_640.png"
+    }
+  ];
+  const headphone = {
+    id: 102,
+    user: "cena@gmail.com",
+    product: "Razer Headphone",
+    price: {
+      value: 99.99,
+      currency: "dollars"
+    }
+  };
+
+  const energyDrink = {
+    id: 77,
+    user: "cena@gmail.com",
+    product: "Monster 500mL",
+    price: {
+      value: 9.99,
+      currency: "dollars"
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +55,11 @@ function App() {
         >
           Learn React
         </a>
+        <Image source="https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg" alternativeText="Cut cat staring" />
+        <h1> Orders recently created </h1>
+          <Order order={headphone} />
+          <Order order={energyDrink} />
+          {users.map(user => <UserProfile user={user} key={user.id}/>)}
       </header>
     </div>
   );
