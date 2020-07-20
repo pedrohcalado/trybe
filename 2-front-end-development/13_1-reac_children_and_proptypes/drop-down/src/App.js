64e888023e741c7b7df90955e8dc6520e41b13fa
pedrohcalado <pedrocalado22@gmail.com>
2020-07-20 09:17:53 -0300

    Exercise 3 and bonus

--- 2-front-end-development/13_1-reac_children_and_proptypes/drop-down/src/App.js
+++ 2-front-end-development/13_1-reac_children_and_proptypes/drop-down/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Button from './Button';
-import Alert from './Alert'
+import Alert from './Alert';
+import DropDown from './DropDown';
+import content from './data';
 
 class App extends Component {
   constructor(props) {
@@ -9,6 +11,8 @@ class App extends Component {
     this.state = {
       showModal: false,
       isDisableButton: false,
+      selectedOption: content[0].item,
+      hideList: true,
     }
   }
 
@@ -25,9 +29,22 @@ class App extends Component {
     ))
   }
 
+  handleList = () => {
+    this.setState(state => ({
+      hideList: !state.hideList,
+    })
+  )}
+
+  handleListElement = (event) => {
+    this.setState({
+      selectedOption: event.target.innerHTML,
+    })
+  }
+
   render() {
     return (
       <div className='main'>
+        <h1>Exercises 01 and 02</h1>
         <Button content="Clique aqui" isDisable={this.state.isDisableButton} showComponent={this.changeShowComponent} value='Título Show' />
         {this.state.showModal &&
           <Alert
@@ -35,6 +52,11 @@ class App extends Component {
             {{title: "Algum título", content: "Algum conteúdoooo", timeSeconds: 3000 }}
           </Alert>
         }
+        <h1>Exercise 03</h1>
+        <DropDown data={content} selectedOption={this.state.selectedOption} hideList={this.state.hideList} onClickListElement={this.handleListElement} onClickSelectedOption={this.handleList}>
+          {/* <h3>Drop down em React mano</h3> */}
+          
+        </DropDown>
       </div>
     )
   }
