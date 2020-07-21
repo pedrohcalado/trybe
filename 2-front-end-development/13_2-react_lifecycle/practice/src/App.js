0a6458488b369bf22a24a07a7996a508ad92ad71
pedrohcalado <pedrocalado22@gmail.com>
2020-07-21 07:28:11 -0300

    Finish practice

--- 2-front-end-development/13_2-react_lifecycle/practice/src/App.js
+++ 2-front-end-development/13_2-react_lifecycle/practice/src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import CounterDisplay from './Counter';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// App.js
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0,
+    };
+  }
+
+  updateStateToTen = () => {
+   this.setState({
+      counter: 10,
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        Contador
+        <button
+          onClick={() => this.setState((state) => ({ counter: state.counter + 1 }))}>
+          Soma
+        </button>
+        <CounterDisplay value={this.state.counter} updateStateToTen={this.updateStateToTen} />
+      </div>
+    );
+  }
 }
 
 export default App;
