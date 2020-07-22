098ee82e02a2b9e88207fd6895e104e7634c0eb0
pedrohcalado <pedrocalado22@gmail.com>
2020-07-22 13:36:11 -0300

    Exercises 1 - 7

--- 2-front-end-development/13_3-react_router/fixation-exercises-13-3/src/App.js
+++ 2-front-end-development/13_3-react_router/fixation-exercises-13-3/src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Home from './Home';
+import About from './About';
+import Users from './Users';
+import { 
+  BrowserRouter,
+  Link,
+  Route,
+  Switch
+ } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/users">Users</Link>
+        <Switch>
+          <Route path="/about" component={About} />
+          <Route path="/users" component={Users} />
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
