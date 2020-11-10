86c5d808be93208ab38ecc7f50b280292157ed4b
pedrohcalado <pedrocalado22@gmail.com>
2020-11-10 11:22:45 -0300

    Exercicio 1

--- 3-back-end-development/26_1-nodeJS_introduction/index.js
+++ 3-back-end-development/26_1-nodeJS_introduction/index.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+/* eslint-disable no-console */
+const readlineSync = require('readline-sync');
+
+const userHeight = readlineSync.questionInt("What's your height in cm? ");
+
+const userWeight = readlineSync.questionFloat("What's your weight in kg? ");
+
+// eslint-disable-next-line no-restricted-properties
+const imc = (userWeight / Math.pow(userHeight * 0.01, 2)).toFixed(2);
+
+console.log(`This is your IMC: ${imc}`);
